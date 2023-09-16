108c69c9b4f253daec38f7565a309b3e34d6b40c
Dmajor81 <christopherleon237@gmail.com>
2023-09-16 05:32:41 +0100

    linear_Search

---
+++ linear_search.py
@@ -0,0 +1,19 @@
+
+def linear_search(list, target):
+    for i in range(0, len(list)):
+        if list[i] == target:
+            return i
+    return None
+
+def verify(Index):
+    if Index is not None:
+        print("The target is at position ", Index)
+    else:
+        print ("Target not found")
+
+numbers=[1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+result=linear_search(numbers, 15)
+verify (result)
+
+result=linear_search(numbers, 10)
+verify (result)
